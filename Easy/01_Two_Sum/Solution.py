2079262e1069caace67dee22e1ee6d1588fdee28
akash-code-byte <akashm.coder@gmail.com>
2026-09-14 17:08:51 +0530

    corrections

--- Easy/01_Two_Sum/Solution.py
+++ Easy/01_Two_Sum/Solution.py
@@ -2,26 +2,13 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        diff_map = dict({})
-        for i in range(len(nums)):
-            diff = target - nums[i]
+        diff_map = {}
+        for i, num in enumerate(nums):
+            diff = target - num
             if(diff in  diff_map.keys()):
                 return [i, diff_map[diff]]
             else:
-                diff_map[nums[i]] = i
-
-
-
-
-        
-
-
-
-
-
-
-
-
+                diff_map[num] = i
 
 obj = Solution()
 print(obj.twoSum([1, 2, 3, 4, 5, 6, 7, 8, 9], int(input("Target: "))))

ee38b4af3d2ebf9c9b15dc4a30130d9a0df28320
akash-code-byte <akashm.coder@gmail.com>
2026-09-14 16:54:06 +0530

    two sum soluiton for python

---
+++ Easy/01_Two_Sum/Solution.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        diff_map = dict({})
+        while(i < len(nums)):
+            diff = target - nums[i]
+            if(diff in  diff_map.keys()):
+                return [i, diff_map[diff]]
+            else:
+                diff_map[nums[i]] = i
+                i+=1
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+obj = Solution()
+print(obj.twoSum([1, 2, 3, 4, 5, 6, 7, 8, 9], int(input("Target: "))))
